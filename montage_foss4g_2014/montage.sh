f84fe2a934ee18507cc6d3e13a56ca16b3021cee
Hugo Mercier <hugo.mercier@oslandia.com>
2014-09-05 09:37:53 +0200

    Update

--- montage_foss4g_2014/montage.sh
+++ montage_foss4g_2014/montage.sh
@@ -5,4 +5,12 @@
 ~/src/misc/gen_title.sh "Display points with bar graphs" titles/title_point_layers.mp4
 ~/src/misc/gen_title.sh "Customizable user actions" titles/title_user_actions.mp4
 ~/src/misc/gen_title.sh "Textured polygons from PostGIS" titles/title_textured_tin.mp4
-mencoder titles/title_textured_tin.mp4 feat_textured_tin.ogv titles/title_point_layers.mp4 feat_points_layer.ogv titles/title_identify.mp4  feat_identify_point.ogv titles/title_user_actions.mp4 feat_user_action.ogv titles/title_lod.mp4 feat_lod.ogv titles/title_multitex.mp4 feat_multitex.ogv -o output.mp4 -ovc x264
+~/src/misc/gen_title.sh "Thematic analysis" titles/title_analysis.mp4
+mencoder titles/title_textured_tin.mp4 feat_textured_tin3.mp4 titles/title_point_layers.mp4 feat_points_layer2.mp4 titles/title_identify.mp4  feat_identify_point2.mp4 titles/title_user_actions.mp4 feat_user_action2.mp4 titles/title_lod.mp4 feat_lod2.mp4 titles/title_multitex.mp4 feat_multitex2.mp4 titles/title_analysis.mp4 feat_analysis2.mp4 -o output.mp4 -ovc x264
+mencoder titles/title_textured_tin.mp4 feat_textured_tin3.mp4 -o cardano_textured_tin.mp4 -ovc x264
+mencoder titles/title_point_layers.mp4 feat_points_layer2.mp4 -o cardano_points_layer.mp4 -ovc x264
+mencoder titles/title_identify.mp4 feat_identify_point2.mp4 -o cardano_identification.mp4 -ovc x264
+mencoder titles/title_user_actions.mp4 feat_user_action2.mp4 -o cardano_user_actions.mp4 -ovc x264
+mencoder titles/title_lod.mp4 feat_lod2.mp4 -o cardano_lod.mp4 -ovc x264
+mencoder titles/title_multitex.mp4 feat_multitex2.mp4 -o cardano_multitextures.mp4 -ovc x264
+mencoder titles/title_analysis.mp4 feat_analysis2.mp4 -o cardano_analysis.mp4 -ovc x264
